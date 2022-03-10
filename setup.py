9704f0a86365d7493651d554a7216c84514ee360
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2022-03-10 12:00:01 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name = "Badboy",
     version = "1.0.0",
-    description = "ls clone",
-    author = "JeffTheK",
+    description = "A evil framework designed purely in Python",
+    author = "AitzazImtiaz",
     url = "https://github.com/AitzazImtiaz/bad-boy",
     packages = ["soli"],
     entry_points = {
