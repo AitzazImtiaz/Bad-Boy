4e9ebdf6a776d65e6ee22da48d1873205d44c22c
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2022-03-10 12:42:40 +0500

    Create installation.py

---
+++ installation.py
@@ -0,0 +1,52 @@
+# License do not apply for this file.
+# Copy it freely for your project.
+# EPL member and a Bad Boy
+# AI or call me Aristodamus Adair
+import time
+import os
+os.system("clear")
+time.sleep(2)
+print("""
+  _   _   _   _   _   _   _   _   _   _   _   _  
+ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ 
+( I | n | s | t | a | l | l | a | t | i | o | n )
+ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ """)
+time.sleep(3)
+print("We are beggining with the Installation")
+time.sleep(3)
+print("Designed by Aitzaz Imtiaz codenamed Aristodamus Adair")
+print ("Bad Boy!")
+time.sleep(3)
+print("Use for Positive purpose!")
+time.sleep(5)
+os.system("clear")
+print("""
+  _   _   _   _     _   _  
+ / \ / \ / \ / \   / \ / \ 
+( M | a | d | e | | B | y )
+ \_/ \_/ \_/ \_/   \_/ \_/ 
+____ _ ___ ___  ____ ___      _ _  _ ___ _ ____ ___  
+|__| |  |    /  |__|   /       | |\/|  |  | |__|   /  
+|  | |  |   /__ |  |  /__      | |  |  |  | |  |  /__ """)
+time.sleep(3)
+print("This Software is dedicated to all my friends. I am here all because of my software partners.")
+time.sleep(4)
+print("Now the Installation of useful packages starts!")
+print("This Software is blocked in both Russia and China. Don't use it if you belong here or prove that you are born for destruction.")
+time.sleep(9)
+os.system("pip install sockets")
+os.system("pip install scapy")
+os.system("pip install hashlib")
+os.system("pip install supyr-struct")
+os.system("pip install colorama")
+os.system("pip install pyftpdlib")
+os.system("pip install pexpect")
+os.system("pip install termcolor")
+os.system("pip install requests")
+os.system("clear")
+print("Now Are you a Bad Boy?")
+print("I like Bad Boys only before police catch them. 😜 ")
+print("Use it ethically Bad Boy or get jailed.")
+print("Sorry if you are Bad Girl because I like Bad Boys only. 😁")
+time.sleep(15)
+os.system("clear")
